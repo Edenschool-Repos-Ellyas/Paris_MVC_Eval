247b968c1b8da4f8d67f8ab70f0e24fdd360ee1b
Ellyas-San <mepereiradebarros@edenschool.fr>
2021-02-26 09:47:51 +0100

    footer creation

--- app/views/inc/footer.php
+++ app/views/inc/footer.php
@@ -1,3 +1,8 @@
+    <div style="height: 50px; background-color: tomato;">
+        I'M THE FOOTER, I RISE LIKE A LORD AND I STILL YOUR HEART
+    </div>
+    
+    
     <!-- Latest compiled and minified JavaScript -->
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
     <script src="<?= URL_ROOT ?>/public/js/app.js"></script>
