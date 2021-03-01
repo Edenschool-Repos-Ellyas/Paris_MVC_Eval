b3262e00a958ea1c49353a0dc566e11d52280e25
TsuTsuKaKushi <goldengames631@gmail.com>
2021-03-01 21:27:21 +0100

    footer began

--- app/views/inc/footer.php
+++ app/views/inc/footer.php
@@ -1,3 +1,7 @@
+    <footer>
+         <marquee><h1>FOOTERRRR !!!! :D</h1></marquee>
+    </footer>
+
     <!-- Latest compiled and minified JavaScript -->
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
     <script src="<?= URL_ROOT ?>/public/js/app.js"></script>
