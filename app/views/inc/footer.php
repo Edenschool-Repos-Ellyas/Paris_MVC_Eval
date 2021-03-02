9aaa9fb59edb7093af86282b8c628d0843e48c96
Ellyas-San <mepereiradebarros@edenschool.fr>
2021-03-02 12:10:52 +0100

    footer V2

--- app/views/inc/footer.php
+++ app/views/inc/footer.php
@@ -1,5 +1,71 @@
     <footer>
-         <marquee><h1>FOOTERRRR !!!!! :D</h1></marquee>
+         <div class="container">
+             <div class="row">
+
+
+                 <div class="col-md-12 col-lg-12 news-letter-container">
+                     <h3><span font-color="orange">SIGN UP</span> FOR THE LATEST NEWS</h3>
+
+                     <form action="" method="post" class="form-news">
+                        <input type="mail" name="news_mail" id="news_mail">
+                        <input type="submit" value="SIGN UP" class="btn" cat-color="orange">
+                     </form>
+
+                 </div>
+             </div>
+
+             <div class="row footer-cat-list">
+                 <div class="col-sm-6 col-md-6 col-lg-3 footer-cat">
+                     <div class="footer-cat-title">
+                         <h5>ABOUT TECH</h5>
+                         <div class="bordure" border-color="blue"></div>
+                     </div>
+                 </div>
+
+                 <div class="col-sm-6 col-md-6 col-lg-3 footer-cat">
+                     <div class="footer-cat-title">
+                         <h5>DISCOVER</h5>
+                         <div class="bordure" border-color="blue"></div>
+                     </div>
+                 </div>
+                 
+                 <div class="col-sm-6 col-md-6 col-lg-3 footer-cat">
+                     <div class="footer-cat-title">
+                         <h5>EDITOR PICKS</h5>
+                         <div class="bordure" border-color="blue"></div>
+                     </div>
+                 </div>
+
+                 <div class="col-sm-6 col-md-6 col-lg-3 footer-cat">
+                     <div class="footer-cat-title">
+                         <h5>TECH PHOTOS</h5>
+                         <div class="bordure" border-color="blue"></div>
+                     </div>
+                 </div>
+
+
+             </div>
+         </div>
+
+         <div class="footer-border">
+             <div>
+                <ul>
+                    <li><i class="fab fa-facebook-f"></i></li>
+                    <li><i class="fab fa-twitter"></i></li>
+                    <li><i class="fab fa-instagram"></i></li>
+                    <li><i class="fab fa-tumblr"></i></li>
+                    <li><i class="fas fa-rss"></i></li>
+                </ul>
+             </div>
+
+             <div>
+                 <h6>&copy; COPYRIGHT 2016-TECH NEWS . DESIGN BY: UICOOKIES</h6>
+             </div>
+
+             <div>
+                 <h6>TECH NEWS MAGAZINE</h6>
+             </div>
+         </div>
     </footer>
 
     <!-- Latest compiled and minified JavaScript -->
