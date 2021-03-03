13d64fea074a316935798871b0f4dcd3ccabde33
Ellyas-San <mepereiradebarros@edenschool.fr>
2021-03-03 16:07:39 +0100

    footer version finished

--- app/views/inc/footer.php
+++ app/views/inc/footer.php
@@ -15,37 +15,132 @@
              </div>
 
              <div class="row footer-cat-list">
-                 <div class="col-sm-6 col-md-6 col-lg-3 footer-cat">
-                     <div class="footer-cat-title">
-                         <h5>ABOUT TECH</h5>
-                         <div class="bordure" border-color="blue"></div>
-                     </div>
-                 </div>
+                    <div class="col-sm-6 col-md-6 col-lg-3 footer-cat">
+                        <div class="footer-cat-title">
+                            <h5>ABOUT TECH</h5>
+                            <div class="bordure" border-color="blue"></div>
+                        </div>
 
-                 <div class="col-sm-6 col-md-6 col-lg-3 footer-cat">
-                     <div class="footer-cat-title">
-                         <h5>DISCOVER</h5>
-                         <div class="bordure" border-color="blue"></div>
-                     </div>
-                 </div>
-                 
-                 <div class="col-sm-6 col-md-6 col-lg-3 footer-cat">
-                     <div class="footer-cat-title">
-                         <h5>EDITOR PICKS</h5>
-                         <div class="bordure" border-color="blue"></div>
-                     </div>
-                 </div>
+                        <img src="<?= URL_ROOT ?>/public/img/img_site/tech_about.jpg" alt="tech about">
+                        <p>Competently conceptualize go forward testing procedures and B2B expertise. Phosfluorescently cultivate principle-centered methods of empowerment through fully researched.
+                        </p>
+                    </div>
+                    <!-- ./col -->
 
-                 <div class="col-sm-6 col-md-6 col-lg-3 footer-cat">
-                     <div class="footer-cat-title">
-                         <h5>TECH PHOTOS</h5>
-                         <div class="bordure" border-color="blue"></div>
-                     </div>
-                 </div>
+                    <div class="col-sm-6 col-md-6 col-lg-3 footer-cat">
+                        <div class="footer-cat-title">
+                            <h5>DISCOVER</h5>
+                            <div class="bordure" border-color="blue"></div>
+                        </div>
 
+                        <div class="list-element">
+                            <ul>
+                                <li><a href="#">Mobile</a></li>
+                                <li><a href="#">Tablet</a></li>
+                                <li><a href="#">Gadgets</a></li>
+                                <li><a href="#">Design</a></li>
+                                <li><a href="#">Camera</a></li>
+                                <li><a href="#">Apps</a></li>
+                                <li><a href="#">Login</a></li>
+                                <li><a href="#">About Us</a></li>
+                                <li><a href="#">Membership</a></li>
+                                <li><a href="#">Blog</a></li>
+                                <li><a href="#">Job</a></li>
+                                <li><a href="#">SiteMap</a></li>
+                            </ul>
+                            <ul>
+                                <li><a href="#">Contact Us</a></li>
+                                <li><a href="#">Newsletter Alerts</a></li>
+                                <li><a href="#">Podcast</a></li>
+                                <li><a href="#">Sms Subscription</a></li>
+                                <li><a href="#">Advertisement Policy</a></li>
+                                <li><a href="#">Report Technical issue</a></li>
+                                <li><a href="#">Complaints and Conditions</a></li>
+                                <li><a href="#">Terms and Conditions</a></li>
+                                <li><a href="#">Privacy Policy</a></li>
+                                <li><a href="#">Cookie Policy</a></li>
+                                <li><a href="#">Securedrop</a></li>
+                                <li><a href="#">Archives</a></li>
+                            </ul>
+                        </div>
+                    </div>
+                    <!-- ./col -->
+
+                    <div class="col-sm-6 col-md-6 col-lg-3 footer-cat">
+                        <div class="footer-cat-title">
+                            <h5>EDITOR PICKS</h5>
+                            <div class="bordure" border-color="blue"></div>
+                        </div>
+
+                        <div class="thumb">
+                            <img src="<?= URL_ROOT ?>/public/img/img_site/editor_pic1.jpg" alt="editor_pic1">
+                            <div>
+                                <h6>Apple launches photo-centric wrist watch for Android</h6>
+                                <ul class="stars">
+                                    <li><i class="fas fa-star"></i></li>
+                                    <li><i class="fas fa-star"></i></li>
+                                    <li><i class="fas fa-star"></i></li>
+                                    <li><i class="fas fa-star"></i></li>
+                                    <li><i class="fad fa-star-half-alt"></i></li>
+                                </ul>
+                            </div>
+                        </div>
+
+                        <div class="thumb">
+                            <img src="<?= URL_ROOT ?>/public/img/img_site/editor_pic2.jpg" alt="editor_pic2">
+                            <div>
+                                <h6>Apple launches photo-centric wrist watch for Android</h6>
+                                <ul class="stars">
+                                    <li><i class="fas fa-star"></i></li>
+                                    <li><i class="fas fa-star"></i></li>
+                                    <li><i class="fas fa-star"></i></li>
+                                    <li><i class="fas fa-star"></i></li>
+                                    <li><i class="fad fa-star-half-alt"></i></li>
+                                </ul>
+                            </div>
+                        </div>
+
+                        <div class="thumb">
+                            <img src="<?= URL_ROOT ?>/public/img/img_site/editor_pic2.jpg" alt="editor_pic2">
+                            <div>
+                                <h6>Apple launches photo-centric wrist watch for Android</h6>
+                                <ul class="stars">
+                                    <li><i class="fas fa-star"></i></li>
+                                    <li><i class="fas fa-star"></i></li>
+                                    <li><i class="fas fa-star"></i></li>
+                                    <li><i class="fas fa-star"></i></li>
+                                    <li><i class="fad fa-star-half-alt"></i></li>
+                                </ul>
+                            </div>
+                        </div>
+
+                    </div>
+                    <!-- ./col -->
+
+                    <div class="col-sm-6 col-md-6 col-lg-3 footer-cat">
+                        <div class="footer-cat-title">
+                            <h5>TECH PHOTOS</h5>
+                            <div class="bordure" border-color="blue"></div>
+                        </div>
+
+                        <div class="tech_photo">
+                            <img src="<?= URL_ROOT ?>/public/img/img_site/tech_photo1.jpg" alt="tech photo 1">
+                            <img src="<?= URL_ROOT ?>/public/img/img_site/tech_photo2.jpg" alt="tech photo 2">
+                            <img src="<?= URL_ROOT ?>/public/img/img_site/tech_photo3.jpg" alt="tech photo 3">
+                            <img src="<?= URL_ROOT ?>/public/img/img_site/tech_photo4.jpg" alt="tech photo 4">
+                            <img src="<?= URL_ROOT ?>/public/img/img_site/tech_photo5.jpg" alt="tech photo 5">
+                            <img src="<?= URL_ROOT ?>/public/img/img_site/tech_photo6.jpg" alt="tech photo 6">
+                            <img src="<?= URL_ROOT ?>/public/img/img_site/tech_photo7.jpg" alt="tech photo 7">
+                            <img src="<?= URL_ROOT ?>/public/img/img_site/tech_photo8.jpg" alt="tech photo 8">
+                            <img src="<?= URL_ROOT ?>/public/img/img_site/tech_photo9.jpg" alt="tech photo 9">
+                        </div>
+
+                    </div>
+                    <!-- ./col -->
 
              </div>
          </div>
+         <!-- ./container -->
 
          <div class="footer-border">
              <div>
@@ -66,6 +161,7 @@
                  <h6>TECH NEWS MAGAZINE</h6>
              </div>
          </div>
+         <!-- ./footer-border -->
     </footer>
 
     <!-- Latest compiled and minified JavaScript -->
