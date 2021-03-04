ef71aa80fc4636250d9c88eac6d6ca7c5beb024f
Ellyas-San <mepereiradebarros@edenschool.fr>
2021-03-04 11:39:11 +0100

    test_nav merged on test_article

--- app/helpers/session_helper.php
+++ app/helpers/session_helper.php
@@ -2,7 +2,7 @@
 	session_start();
 	/**
 	 * @return bool
-	 * Check si l'utilisateur est connecté via le cookie dee seession
+	 * Check si  l'utilisateur est connecté via le cookie dee seession
 	 */
 	function isLoggedIn() {
 		if (isset($_SESSION['user_id'])) {
