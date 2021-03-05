ee11d4d151ba5a81593d4312eaf54d076b1c2064
Ellyas-San <mepereiradebarros@edenschool.fr>
2021-03-05 16:55:41 +0100

    push de test fin de day

---
+++ public/js/search.js
@@ -0,0 +1,39 @@
+const searchInput = document.getElementById("search");
+
+search.addEventListener("keyup", getArticleHint);
+
+function getArticleHint(str) {
+    
+    str = searchInput.value;
+    str = str.trim();
+    if(str === "") return;
+
+    let xhr = new XMLHttpRequest();
+    xhr.responseType = "json";
+
+    xhr.onreadystatechange = () => {
+        if (this.readyState === 4 && this.status === 200) {
+            
+            let articles = this.response;
+            let resultContainer = document.getElementById("search-container");
+            let html = "";
+
+            articles.map((article)=>{
+                let contentHtml = `
+                    <div class="search-results">
+                        <h5><a href="http://localhost/Paris_MVC_Eval/articles/show/${article.art_id}">${article.title}</a></h5>
+                    </div>
+                `;
+
+                html.innerHTML += contentHtml;
+            });
+
+            resultContainer.innerHTML = html;
+        }
+    }
+
+    xhr.open("GET", "http://localhost/Paris_MVC_Eval/ajaxs/hintAjax/" + str);
+    xhr.send();
+}
+
+// bidule
